42a2bc0a3b343eb3930b3ddacdb3f981e2cbe6c7
Gabriel Stackhouse <gabes@iastate.edu>
2019-04-13 13:11:24 -0500

    addCommunication() is ignored after createGraph() is called

--- test/CommunicationsMonitorTest.java
+++ test/CommunicationsMonitorTest.java
@@ -1,10 +1,31 @@
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CommunicationsMonitorTest {
 
+    // Instance variables
+    CommunicationsMonitor monitor;
+
+    @Before
+    public void initialize() {
+        monitor = new CommunicationsMonitor();
+    }
+
     @Test
     public void addCommunication() {
+        // Initial size should be zero
+        assertEquals(0, monitor.getCommunicationList().size());
+
+        // Insert a tuple
+        monitor.addCommunication(1, 2, 3);
+        assertEquals(2, monitor.getCommunicationList().size());
+
+        // Shouldn't work after createGraph() is called
+        monitor.createGraph();
+        monitor.addCommunication(5, 6, 7);
+        assertEquals(2, monitor.getCommunicationList().size());
     }
 
     @Test
