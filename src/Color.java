6c8bc1db5941c956be303f5da789be98d57dffac
Jackson Scholberg <j3json@gmail.com>
2019-04-24 11:33:17 -0500

    Documentation for CommunicationTriple

--- src/Color.java
+++ src/Color.java
@@ -1,5 +1,9 @@
 /**
  * Enumerator for color of nodes. Used in BFS
+ *
+ * @author Gabriel Stackhouse
+ * @author Jackson Scholberg
+ * @author Zachary Gorman
  */
 public enum Color {
     WHITE, GREY, BLACK;
